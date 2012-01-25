eb7e67127371f7add82c4005736a014f82164198
kailash kumar <kailashk06@gmail.com>
2012-01-26 01:30:23 +0530

    depot187 validation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot187::Application.config.secret_token = '9356229aac96f11be22dcd44ed8be17dcd959881fa5827d63cc62aa181460e7c90c6ca3bcf2be49c3db2527aa4f2328d2100a596e98c5b0e5482334fe721d954'
